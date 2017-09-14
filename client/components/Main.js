cd0d354454f5869145d4eb4acb592686da624dc4
Camden Bickel <cam@bickel.io>
2017-09-14 22:49:55 +0100

    Added addtl variants

--- client/components/Main.js
+++ client/components/Main.js
@@ -14,6 +14,10 @@ resume.basics.location.city = process.env.LOCATION_CITY;
 resume.basics.location.city = process.env.LOCATION_CITY;
 resume.basics.location.region = process.env.LOCATION_REGION;
 resume.basics.location.regionAbbrev = process.env.LOCATION_REGION_ABBREV;
+resume.variants.uk.basics.phone = process.env.VARIANT_UK_PHONE_NUMBER;
+resume.variants.uk.basics.location.city = process.env.VARIANT_UK_LOCATION_CITY;
+resume.variants.uk.basics.location.regionAbbrev =
+  process.env.VARIANT_UK_LOCATION_REGION_ABBREV;
 
 class Main extends Component {
   state: {
